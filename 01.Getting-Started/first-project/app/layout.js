19a0c1f7dbba31a8ca774e988b3bb00735a41464
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-29 08:44:10 +0530

    Improvement 03

--- 01.Getting-Started/first-project/app/layout.js
+++ 01.Getting-Started/first-project/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/component/Navbar";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-300`}>
         <Navbar />
         {children}
+
+        <div className="flex gap-4 w-max px-5 py-2 bg-amber-500 mt-4">
+          <Link href={"/"}>Home Page</Link>
+          <Link href={"/contact"}>Contact Page</Link>
+          <Link href={"/about"}>About Page</Link>
+          <Link href={"/about/skills"}>Skills Page</Link>
+        </div>
       </body>
     </html>
   );
--- 01.Getting-Started/first-project/component/Navbar.js
+++ 01.Getting-Started/first-project/component/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='bg-amber-500 px-3 py-2'>
+    <div className='bg-blue-800 text-white px-3 py-2'>
       I'm Navbar
     </div>
   )
