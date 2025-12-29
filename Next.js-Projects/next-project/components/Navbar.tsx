ae4eb6014a460ec9484a2e7d4cf9bf192c505faf
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-29 18:14:20 +0530

    Improvement 04

---
+++ Next.js-Projects/next-project/components/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar;
