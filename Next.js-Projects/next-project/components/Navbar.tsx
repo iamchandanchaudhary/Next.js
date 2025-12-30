5f7ab984b4bdd8765e348d920ad1cd7ab4255c01
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-30 08:53:51 +0530

    making navbar

--- Next.js-Projects/next-project/components/Navbar.tsx
+++ Next.js-Projects/next-project/components/Navbar.tsx
@@ -5,9 +5,32 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 function Navbar({ className }: { className?: string }) {
+
+  const [active, setActive] = useState<string | null>(null);
+
   return (
-    <div>
-      Navbar
+    <div className={cn("fixed top-10 bg-amber-500 dark:bg-blue-500 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home">
+            
+          </MenuItem>
+        </Link>
+
+        <Link href={"/services"}>
+          <MenuItem setActive={setActive} active={active} item="Services">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/web-dev">Web Development</HoveredLink>
+              <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+              <HoveredLink href="/branding">Branding</HoveredLink>
+            </div>
+          </MenuItem>
+        </Link>
+
+      </Menu>
+
     </div>
   )
 }
