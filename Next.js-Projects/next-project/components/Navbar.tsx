6b4c717c5470212c2b61c79e90c28ff67f789007
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-12-30 16:56:46 +0530

    Navbar Improvement

--- Next.js-Projects/next-project/components/Navbar.tsx
+++ Next.js-Projects/next-project/components/Navbar.tsx
@@ -1,50 +1,47 @@
 "use client";
 import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-import { FloatingNav } from "./ui/floating-navbar";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+function Navbar({ className }: { className?: string }) {
 
-const NavbarMenu = () => {
-
-  const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "About",
-      link: "/about",
-      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Contact",
-      link: "/contact",
-      icon: (
-        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-      ),
-    },
-  ];
+  const [active, setActive] = useState<string | null>(null);
 
   return (
-    <div className="relative  w-full">
-      <FloatingNav navItems={navItems} />
-      {/* <DummyContent /> */}
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home">
+            
+          </MenuItem>
+        </Link>
+
+        <Link href={"/services"}>
+          <MenuItem setActive={setActive} active={active} item="Services">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/web-dev">Web Development</HoveredLink>
+              <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+              <HoveredLink href="/branding">Branding</HoveredLink>
+            </div>
+          </MenuItem>
+        </Link>
+
+        <Link href={"/contact"}>
+          <MenuItem setActive={setActive} active={active} item="Contact">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/call">Call</HoveredLink>
+              <HoveredLink href="/email">Email</HoveredLink>
+            </div>
+          </MenuItem>
+        </Link>
+
+      </Menu>
+
     </div>
   )
 }
 
-export default NavbarMenu;
-
-// const DummyContent = () => {
-//   return (
-//     <div className="grid grid-cols-1 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md">
-//       <p className="dark:text-white text-neutral-600 text-center text-4xl mt-40 font-bold">
-//         Scroll back up to reveal Navbar
-//       </p>
-//       <div className="inset-0 absolute bg-grid-black/[0.1] dark:bg-grid-white/[0.2]" />
-//     </div>
-//   );
-// };
+export default Navbar;
