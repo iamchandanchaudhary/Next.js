f42940e58ddd216807a974afa4ab64b6dd6f324a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-01-01 09:05:28 +0530

    Upcoming Events Section

--- Next.js-Projects/next-project/app/page.tsx
+++ Next.js-Projects/next-project/app/page.tsx
@@ -1,16 +1,18 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingEvents from "@/components/UpcomingEvents";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="min-h-screen bg-black/96 antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
       <TestimonialCards />
+      <UpcomingEvents />
     </main>
   );
 }
