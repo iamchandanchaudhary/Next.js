d6979cb2c691a763265a7de9f29b0e3d4a1101de
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-01-09 10:32:13 +0530

    mystery message update

--- Next.js-Projects/mystery-message/src/model/User.model.ts
+++ Next.js-Projects/mystery-message/src/model/User.model.ts
@@ -64,3 +64,5 @@ const UserSchema: Schema<User> = new Schema({
     },
     messages: [MessageSchema]
 })
+
+const userModel = (mongoose.models.User as mongoose.Model<User>) ||  mongoose.model<User>("User", UserSchema);
