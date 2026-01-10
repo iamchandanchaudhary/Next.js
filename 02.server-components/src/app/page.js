248f4f3252b4b4a16100c2cf36f8ab2313251bc3
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-01-10 07:05:37 +0530

    server component

---
+++ 02.server-components/src/app/page.js
@@ -0,0 +1,26 @@
+"use client" // ==> It will allow us to run our next app on client side but can't show server side details
+// because next.js is by default run on server side
+import { useState, useEffect } from "react";
+// import fs from "fs/promises";
+
+export default function Home() {
+
+  const [count, setCount] = useState(0);
+  console.log("Hello Chandan");
+
+  // let a = fs.readFile(".gitignore");
+  // a.then((e) => console.log(e.toString())) // ==> it was a server side data can't run on client side
+
+  return (
+    <div className="p-4">
+      <p>Total: {count}</p>
+
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-white hover:bg-white/80 rounded text-black px-2 py-0.5"
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
