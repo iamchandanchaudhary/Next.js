56ffb393c8c806b47e0ccab4ae56dc187573c420
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-01-10 06:11:59 +0530

    Changing Structure

---
+++ 03.Script, Link and Image Components/components-project/next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        port: '',
+        pathname: '/my-bucket/**',
+        search: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
